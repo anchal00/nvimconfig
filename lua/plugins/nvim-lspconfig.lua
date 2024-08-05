c9317b0b4db95add38f4b4d28d69bf4c35bfb8ae
anchal00 <anchal82199@gmail.com>
2024-08-05 20:23:48 +0530

    Add autocomplete, python lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,32 +1,57 @@
-local config = function()
-  -- require("neoconf").setup({})
-  -- local cmp_nvim_lsp = require("cmp_nvim_lsp")
-  local lspconfig = require "lspconfig"
+local on_attach = require("util.lsp").on_attach
+local diagnostic_signs = require("util.lsp").diagnostic_signs
 
-  local diagnostic_signs = { Error = " ", Warn = " ", Hint = "", Info = "" }
+local config = function()
+  require("neoconf").setup {}
+  local lspconfig = require "lspconfig"
+  local cmp_nvim_lsp = require "cmp_nvim_lsp"
+
   for type, icon in pairs(diagnostic_signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
   end
 
-  -- local capabilities = cmp_nvim_lsp.default_capabilities()
+  local capabilities = cmp_nvim_lsp.default_capabilities()
 
   -- lua
   lspconfig.lua_ls.setup {
     -- capabilities = capabilities,
-    -- on_attach = on_attach,
+    on_attach = on_attach,
     settings = { -- custom settings for lua
       Lua = {
         -- make the language server recognize "vim" global
         diagnostics = {
           globals = { "vim" },
         },
+        -- workspace = {
+        --   -- make language server aware of runtime files
+        --   library = {
+        --     vim.fn.expand "$VIMRUNTIME/lua",
+        --     vim.fn.stdpath("config") .. "/lua"),
+        --   },
+        -- },
         workspace = {
-          -- make language server aware of runtime files
           library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.stdpath "config" .. "/lua"] = true,
+            vim.fn.expand "$VIMRUNTIME/lua",
+            vim.fn.expand "$XDG_CONFIG_HOME" .. "/nvim/lua",
           },
+        },
+      },
+    },
+  }
+
+  -- python
+  lspconfig.pyright.setup {
+    -- capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      pyright = {
+        disableOrganizeImports = false,
+        analysis = {
+          useLibraryCodeForTypes = true,
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          autoImportCompletions = true,
         },
       },
     },
@@ -35,6 +60,9 @@ local config = function()
   local luacheck = require "efmls-configs.linters.luacheck"
   local stylua = require "efmls-configs.formatters.stylua"
 
+  local black = require "efmls-configs.formatters.black"
+  local mypy = require "efmls-configs.linters.mypy"
+  local flake8 = require "efmls-configs.linters.flake8"
   -- configure efm server
   lspconfig.efm.setup {
     filetypes = {
@@ -64,7 +92,7 @@ local config = function()
     settings = {
       languages = {
         lua = { luacheck, stylua },
-        -- python = { flake8, black },
+        python = { flake8, black, mypy },
         -- typescript = { eslint_d, prettierd },
         -- json = { eslint_d, fixjson },
         -- jsonc = { eslint_d, fixjson },
@@ -80,21 +108,6 @@ local config = function()
       },
     },
   }
-
-  -- auto-format on save
-  local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    group = lsp_fmt_group,
-    callback = function()
-      local efm = vim.lsp.get_clients { name = "efm" }
-
-      if vim.tbl_isempty(efm) then
-        return
-      end
-
-      vim.lsp.buf.format { name = "efm" }
-    end,
-  })
 end
 
 return {
@@ -105,8 +118,8 @@ return {
     "windwp/nvim-autopairs",
     "williamboman/mason.nvim",
     "creativenull/efmls-configs-nvim",
-    -- "hrsh7th/nvim-cmp",
-    -- "hrsh7th/cmp-buffer",
-    -- "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
   },
 }
